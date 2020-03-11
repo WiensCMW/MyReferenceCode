ee72023776b4629e65fb26db32c2dc0df6057fbb
cornie <cornie.wiens@vidir.com>
2020-03-11 17:43:13 -0500

    Added SQL linebreaks

--- SQL/SQL Error Handling.sql
+++ SQL/SQL Error Handling.sql
@@ -1,8 +1,14 @@
 /* Simple catch and return of error code via output parm */
 DECLARE @OutputMessage NVARCHAR(MAX) --OUTPUT
 BEGIN TRY
-	--....
+    SELECT 'do stuff...'
 END TRY
 BEGIN CATCH
     SET @OutputMessage = ERROR_MESSAGE()
 END CATCH
+
+/*	Adding linebreaks 
+	char(13)+char(10) for CRLF style
+	CHAR(13) for CR style
+*/
+SELECT 'This is line 1.' + CHAR(13) + CHAR(10) + 'This is line 2.'
