6c50c3b7012a9ffc1c2baa3dc2cb3aa8601049c3
Cornelius Wiens <cmw1334@gmail.com>
2020-10-29 22:49:03 -0500

    Parser now removes extra escaped double quotes, but still has issues

--- c-sharp/CSVParser/CSVParser/CParser.cs
+++ c-sharp/CSVParser/CSVParser/CParser.cs
@@ -10,19 +10,12 @@ namespace CSVParser
     {
         public static void CSVParse()
         {
-            //string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"CSV Data.csv");
             string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), @"CSV Data.csv");
-            string[] csvData = File.ReadAllLines(path);
-
-            string data = ""; //SVSampleData();
-            data = string.Join(",", csvData);
-            //var test = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string[] csvStringArrayData = File.ReadAllLines(path);
+            string csvData = string.Join(",", csvStringArrayData);
 
             // List that will hold the split data
             List<string> splitData = new List<string>();
-
-            // List that will hold the incides of the dropped escaped chars while parsing
-            List<int> droppedEscapes = new List<int>();
 
             // Parse bools
             bool csvDataCurrupt = false;
@@ -38,13 +31,12 @@ namespace CSVParser
             int valueEndIndex = 0;
 
             // Loop over data and keep splitting by delimiter
-            while (data.Length > 0 && !csvDataCurrupt)
+            while (csvData.Length > 0 && !csvDataCurrupt)
             {
                 // Reset parse variables
                 inDoubleQuotationMode = false;
                 int charIndex = 0;
                 valueEndIndex = -1;
-                droppedEscapes.Clear();
 
                 #region Loop through data to find start and end incides for next value to parse
                 while (true)
@@ -56,9 +48,9 @@ namespace CSVParser
                     #endregion
 
                     #region Evalutate current loop char
-                    lastChar = charIndex == data.Length - 1;
-                    curCharIsQuote = data.Substring(charIndex, 1) == "\"";
-                    curCharIsDelimiter = data.Substring(charIndex, 1) == ",";
+                    lastChar = charIndex == csvData.Length - 1;
+                    curCharIsQuote = csvData.Substring(charIndex, 1) == "\"";
+                    curCharIsDelimiter = csvData.Substring(charIndex, 1) == ",";
                     #endregion
 
                     #region Check for curruption
@@ -74,14 +66,14 @@ namespace CSVParser
                     // If not at last char, get next char
                     if (!lastChar)
                     {
-                        nextCharIsQuote = data.Substring(charIndex + 1, 1) == "\"";
-                        nextCharIsDelimiter = data.Substring(charIndex + 1, 1) == ",";
+                        nextCharIsQuote = csvData.Substring(charIndex + 1, 1) == "\"";
+                        nextCharIsDelimiter = csvData.Substring(charIndex + 1, 1) == ",";
                     }
 
                     // If we have at least two more chars left, get the char after next
-                    if ((data.Length - 1) - charIndex >= 2)
+                    if ((csvData.Length - 1) - charIndex >= 2)
                     {
-                        nextCharIsDelimiterAfterQuote = data.Substring(charIndex + 2, 1) == ",";
+                        nextCharIsDelimiterAfterQuote = csvData.Substring(charIndex + 2, 1) == ",";
                     }
                     #endregion
 
@@ -132,26 +124,26 @@ namespace CSVParser
 
                 #region Extract substring from data based on end index found in preceeding loop
                 string extractedValue = "";
-                if (valueEndIndex >= 0 && valueEndIndex <= data.Length - 1)
+                if (valueEndIndex >= 0 && valueEndIndex <= csvData.Length - 1)
                 {
                     // Extract value from data based on calculated end index
-                    extractedValue = data.Substring(0, valueEndIndex + 1);
+                    extractedValue = csvData.Substring(0, valueEndIndex + 1);
 
                     // Remove extracted data from data string
-                    if (extractedValue.Length == data.Length)
+                    if (extractedValue.Length == csvData.Length)
                     {
-                        data = "";
+                        csvData = "";
                     }
                     else
                     {
                         int start = extractedValue.Length;
-                        int length = data.Length - extractedValue.Length;
+                        int length = csvData.Length - extractedValue.Length;
 
-                        data = data.Substring(start, length);
+                        csvData = csvData.Substring(start, length);
                     }
                 }
 
-                // Remove trailing delimiter
+                #region Remove trailing delimiter from extracted value
                 if (extractedValue.Length > 1)
                 {
                     if (extractedValue.Substring(extractedValue.Length - 1, 1) == ",")
@@ -159,6 +151,57 @@ namespace CSVParser
                         extractedValue = extractedValue.Substring(0, extractedValue.Length - 1);
                     }
                 }
+                #endregion
+
+                #region Remove encasing double quotes
+                if (extractedValue.Length >= 3)
+                {
+                    if (extractedValue.Substring(0, 1) == "\""
+                        && extractedValue.Substring(extractedValue.Length - 1, 1) == "\"")
+                    {
+                        // Remove double quote from start of extracted
+                        extractedValue = extractedValue.Substring(1, extractedValue.Length - 1);
+
+                        // Remove double quote from end of extracted
+                        extractedValue = extractedValue.Substring(0, extractedValue.Length - 1);
+                    }
+                }
+                #endregion
+
+                #region Remove extra escaped double quotes
+                if (extractedValue.Length >= 1)
+                {
+                    charIndex = 0;
+                    while (true)
+                    {
+                        #region Evalutate current and next loop chars
+                        lastChar = charIndex == extractedValue.Length - 1;
+                        curCharIsQuote = extractedValue.Substring(charIndex, 1) == "\"";
+                        nextCharIsQuote = false;
+
+                        if (!lastChar)
+                        {
+                            nextCharIsQuote = extractedValue.Substring(charIndex + 1, 1) == "\"";
+                        }
+                        #endregion
+
+                        // If current and next chars are both double quotes, get rid of one
+                        if (curCharIsQuote && nextCharIsQuote)
+                        {
+                            // Remove current loop char
+                            extractedValue = extractedValue.Remove(charIndex, 1);
+
+                            // Recheck if we're on he last char after removing a char
+                            lastChar = charIndex == extractedValue.Length - 1;
+                        }
+
+                        if (lastChar)
+                            break;
+                        else
+                            charIndex++;
+                    }
+                }
+                #endregion
 
                 // Add extracted value to results
                 splitData.Add(extractedValue);
@@ -170,11 +213,6 @@ namespace CSVParser
             {
                 Console.WriteLine(splitData[i]);
             }
-        }
-
-        public static string CSVSampleData()
-        {
-            return $"One,Two";
         }
     }
 }
